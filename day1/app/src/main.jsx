8ef527c0b6e7cb07ecd597f5d1b9b08ee8d8bf6a
indhuyadav <indhuy88@gmail.com>
2025-02-06 13:40:25 +0530

    files

--- day1/app/src/main.jsx
+++ day1/app/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'  
-import State1 from './state/State1'
+// import './index.css'  
+// import State1 from './state/State1'
+// import Inline from './styles/Inline'
+import Comp1 from './styles/Comp1'
 // import Cbc from './component/Cbc.jsx'
 // import Fbc from './component/Fbc.jsx'
 // import Demo from './component/Demo.jsx'
 // import Parent from './props/Parent.jsx'
 // import State from './state/State.jsx'
+import "../src/styles/globally.css"
+import Comp2 from './styles/Comp2'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +19,9 @@ createRoot(document.getElementById('root')).render(
     <Demo/> */}
     {/* <Parent/> */}
     {/* <State/> */}
-    <State1/>
+    {/* <State1/> */}
+    {/* <Inline/> */}
+    <Comp1/>
+    <Comp2/>
   </StrictMode>,
 )
---
+++ day1/app/src/styles/Inline.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inline = () => {
+  return (
+    <>
+      <h1 style={{backgroundColor:"green", textAlign:"center", textDecoration:"underline"}}>Hello-World</h1>
+    </>
+  )
+}
+
+export default Inline
