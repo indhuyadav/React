e0b4c2dab885dac57ef4eb01e87b5a0788d2d727
indhuyadav <indhuy88@gmail.com>
2025-02-12 13:01:06 +0530

    msg

--- day1/app/src/useeffect/UseEffect.jsx
+++ day1/app/src/useeffect/UseEffect.jsx
@@ -52,24 +52,71 @@
 //-------------------------------------------------------------
 //dependency with value
 
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 
+// const UseEffect = () => {
+//     let [state, setState] =useState(0)
+//     let [increment, setIncrement]=useState(0)
+
+//     useEffect(()=>{
+//         setIncrement(()=> state*2)
+//     }, [state])
+
+//   return (
+//     <div>
+//       {state}
+//       <button onClick={()=> setState((e)=> e+1)}>click</button>
+//       {increment}
+//     </div>
+//   )
+// }
+
+// export default UseEffect
+
+//-----------------------------------------------------------
+
+//FETCH API using useEffect hook
+
+
+import React, { useState,useEffect } from 'react'
+import "./useeffect.css"
 const UseEffect = () => {
-    let [state, setState] =useState(0)
-    let [increment, setIncrement]=useState(0)
+  let [state,setState] = useState([])
+  console.log(state);
 
-    useEffect(()=>{
-        setIncrement(()=> state*2)
-    }, [state])
-
+  useEffect( ()=>{
+    window.fetch("https://fakestoreapi.com/products")
+    .then((data)=> data.json())
+    .then((res)=> setState(res))
+  },[])
   return (
     <div>
-      {state}
-      <button onClick={()=> setState((e)=> e+1)}>click</button>
-      {increment}
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>login</th>
+            <th>image</th>
+          </tr>
+        </thead>
+        {
+          state.map((res)=>{
+            console.log(res);
+            return(
+              <>
+              <tr>
+                <td>{res.id}</td>
+                <td>{res.price}</td>
+                <td><img src={res.image} alt=""/></td>
+              </tr>
+              </>
+            )
+          })
+        }
+      </table>
     </div>
   )
 }
 
-export default UseEffect
 
+export default UseEffect;
